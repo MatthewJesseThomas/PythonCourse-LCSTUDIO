f820bc3ed1cf33aa7e8a362adec4621a3e0af1e6
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-07-14 09:24:47 +0200

    Python Basics

---
+++ DataTypes/main.py
@@ -0,0 +1,98 @@
+# # Variables
+# fullname = "Matthew Thomas" #fullname Variable
+# age = 23 # age Variable
+# # print(f"User {fullname} + is {age} years old.")
+# #print(type(fullname) == str) #print(type(fullname))
+# #print(type(age)  == int) #print(type(int))
+# #print(isinstance(fullname, str)) #Evaluates 'str' to -> True
+# #print(isinstance(age, int)) #Evaluates 'int' to -> True
+# # print(isinstance(age, float)) #Evaluates 'float' to -> True
+# =============================================
+# # Operators
+# op_1 = 1 + 1
+# op_2 = 2 - 1
+# op_3 = 2 * 2
+# op_4 = 4 / 2
+# op_5 = 4 % 3
+# op_6 = 4 ** 2
+# op_7 = 4 // 2
+# # Comparison Operators
+# a = 10
+# b = 53
+# a == b #False
+# a != b #True
+# a > b #False
+# a <= b #True
+# ================================================
+# # Boolean Operators
+# condition1 = True
+# condition2 = False
+# not condition1 #False
+# condition1 and condition2 #False
+# condition1 or condition2 #
+# print(0 or 1) #1
+# print(False or 'Hello Matthew') #'Hello Matthew'
+# print('Hello Friend' or 'Hello Matthew Jesse Thomas') #'HelloFirend'
+# print([] or False) #'False'
+# print(False or []) #'[]'
+# print(0 and 1) #0
+# print(1 and 0) #0
+# print(False and 'Hello Matthew') #'False'
+# print('Hello Friend' and 'Hello Matthew Jesse Thomas') #'Hello Matthew Jesse Thomas'
+# print([] and False) #'[]'
+# print(False and []) #'False'
+# Ternary Operator
+# mage = input('What Mage Art Thou: ')
+# arts = ["Necromancer", "Destruction", "Alteration"]
+# def is_magic(mage):
+#     if mage == 'Necromancer':
+#         return True
+#     else:
+#         return False
+      
+# def is_magic2(mage):
+#     return True if mage == 'Necromancer' else False
+# ==========================================
+# String 
+# print("""Matthew Is
+
+# The Supreme
+
+# Ruler of Earth
+# """)
+# name = 'Be\"thesda' #Escape String '\'
+# name2 = 'Be\nthesda' #\N new line
+# print(name)
+# print(name2)
+# print(name.lower())
+# print("da" in name)
+# my_string = "Omyodo Jujutsu"
+# print(my_string) #Prints Omyodo Jujutsu
+# print(my_string[6:]) #Prints Jujutsu
+# task_completed = True
+# task_completed = False
+# hellvitica = "Death-Magra"
+# hellvitica_warrior = "Capture 3 Bears"
+# tasks_completed = str(1)
+
+# print(type(task_completed==str) == bool) #made variable == str to have bool == True
+# print(type(task_completed) == bool)
+
+# if task_completed:
+#   print(f"Hellvitica Task {hellvitica} II is Completed")
+# else:
+#   print(f"Hellvitica Task {hellvitica_warrior} I is Incompleted ")
+# ===============================================
+# Boolean Choice
+# harry_potter = True
+# twilight = False
+# Checks if any Read Any Books is True == True
+# read_any_book = any([harry_potter, twilight])
+# print(read_any_book)
+# Example 2
+# Boolean Choice
+# lasagna = True
+# pasta = True
+# Checks if all Read To Serve, if all value is False == False but if all True == True
+# ready_to_serve = all([lasagna, pasta])
+# print(ready_to_serve)
